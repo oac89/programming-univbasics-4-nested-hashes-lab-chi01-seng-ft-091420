53823c4ed23f1675ffba3c25bb6792d9daa2a9e4
Oscar A Castro <oacastro2@gmail.com>
2020-08-29 01:59:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,7 +106,7 @@ def changing_alan
          :languages => ["C"]
        }
      }
-  programmer_hash[:alan_kay][:known_for] = "GUI"
+  return programmer_hash[:alan_kay][:known_for] = "GUI"
 
  end
 
