5b952a88a3508aef4464bbfdb8eeffc957bac5c8
Oscar A Castro <oacastro2@gmail.com>
2020-08-29 01:57:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -90,27 +90,25 @@ end
 
  
 
-# # def changing_alan
-# #   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-# #   # return the entire updated hash
+def changing_alan
+  programmer_hash = 
+ 		{
+       :grace_hopper => {
+         :known_for => "COBOL",
+         :languages => ["COBOL", "FORTRAN"]
+       },
+       :alan_kay => {
+         :known_for => "Object Orientation",
+         :languages => ["Smalltalk", "LISP"]
+      },
+       :dennis_ritchie => {
+         :known_for => "Unix",
+         :languages => ["C"]
+       }
+     }
+  programmer_hash[:alan_kay][:known_for] = "GUI"
 
-# # 	programmer_hash = 
-# # 		{
-# #       :grace_hopper => {
-# #         :known_for => "COBOL",
-# #         :languages => ["COBOL", "FORTRAN"]
-# #       },
-# #       :alan_kay => {
-# #         :known_for => "Object Orientation",
-# #         :languages => ["Smalltalk", "LISP"]
-# #       },
-# #       :dennis_ritchie => {
-# #         :known_for => "Unix",
-# #         :languages => ["C"]
-# #       }
-# #     }
-
-# # end
+ end
 
 # # def adding_to_dennis
 # #   # add "Assembly" to Dennis Ritchie's languages array
